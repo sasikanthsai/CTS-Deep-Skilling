ad77053110390baaf9f95e3134461c97bbe4ec95
sasikanthsai <128284394+sasikanthsai@users.noreply.github.com>
2026-06-16 11:02:58 +0530

    Add files via upload

---
+++ EF-Core-8.0/Lab-5-Retrieve-Data./Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventory.Data;
+
+using var context = new AppDbContext();
+
+// Retrieve all products
+var products = await context.Products.ToListAsync();
+
+foreach (var p in products)
+{
+    Console.WriteLine($"{p.Name} - ₹{p.Price}");
+}
+
+// Find product by ID
+var product = await context.Products.FindAsync(1);
+
+Console.WriteLine($"Found: {product?.Name}");
+
+// First product with price > 50000
+var expensive = await context.Products
+    .FirstOrDefaultAsync(p => p.Price > 50000);
+
+Console.WriteLine($"Expensive: {expensive?.Name}");
