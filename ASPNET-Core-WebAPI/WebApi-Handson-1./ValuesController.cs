d398b998439a6659dda1a4fdb05248fa9bd7c1ba
sasikanthsai <128284394+sasikanthsai@users.noreply.github.com>
2026-06-16 11:37:20 +0530

    Add files via upload

---
+++ ASPNET-Core-WebAPI/WebApi-Handson-1./ValuesController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiHandson.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ValuesController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new[] { "Value1", "Value2" });
+    }
+
+    [HttpPost]
+    public IActionResult Post()
+    {
+        return Ok("Data Added");
+    }
+
+    [HttpPut]
+    public IActionResult Put()
+    {
+        return Ok("Data Updated");
+    }
+
+    [HttpDelete]
+    public IActionResult Delete()
+    {
+        return Ok("Data Deleted");
+    }
+}
