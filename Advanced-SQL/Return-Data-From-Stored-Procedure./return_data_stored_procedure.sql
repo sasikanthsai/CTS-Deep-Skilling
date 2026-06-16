58285fe89570c6d7cebdfd2f8207fb556e0528e4
sasikanthsai <128284394+sasikanthsai@users.noreply.github.com>
2026-06-16 08:30:28 +0530

    Add files via upload

---
+++ Advanced-SQL/Return-Data-From-Stored-Procedure./return_data_stored_procedure.sql
@@ -0,0 +1,8 @@
+CREATE PROCEDURE sp_GetEmployeeCountByDepartment
+    @DepartmentID INT
+AS
+BEGIN
+    SELECT COUNT(*) AS TotalEmployees
+    FROM Employees
+    WHERE DepartmentID = @DepartmentID;
+END;
