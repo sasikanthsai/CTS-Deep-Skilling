c9b0e315bb35ae4c63a58cdecc85d71870e301b8
sasikanthsai <128284394+sasikanthsai@users.noreply.github.com>
2026-06-17 09:09:19 +0530

    Add files via upload

---
+++ ASPNET-Core-WebAPI/WebApi-Handson-2./Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Swagger Services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Swagger Middleware
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
