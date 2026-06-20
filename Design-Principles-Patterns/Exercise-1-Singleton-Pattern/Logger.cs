9aceeddea34c18a3560b2a12dc2d58b4a5184b74
sasikanthsai <128284394+sasikanthsai@users.noreply.github.com>
2026-06-20 09:30:56 +0530

    Add files via upload

---
+++ Design-Principles-Patterns/Exercise-1-Singleton-Pattern/Logger.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Logger
+{
+    private static Logger instance;
+
+    private Logger()
+    {
+        Console.WriteLine("Logger instance created");
+    }
+
+    public static Logger GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Logger();
+        }
+
+        return instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine("LOG: " + message);
+    }
+}
