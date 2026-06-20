c1cbf309e004e295c254b556a944e498d2f4df60
sasikanthsai <128284394+sasikanthsai@users.noreply.github.com>
2026-06-20 08:52:57 +0530

    Add files via upload

---
+++ Design-Principles-Patterns/Exercise-1-Singleton-Pattern/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Logger
+{
+    private static Logger instance;
+
+    private Logger()
+    {
+        Console.WriteLine("Logger instance created");
+    }
+
+    public static Logger GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Logger();
+        }
+
+        return instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine("LOG: " + message);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger logger1 = Logger.GetInstance();
+        Logger logger2 = Logger.GetInstance();
+
+        logger1.Log("First message");
+        logger2.Log("Second message");
+
+        Console.WriteLine(
+            "Both objects are same: " +
+            Object.ReferenceEquals(logger1, logger2));
+    }
+}
