3934753b08099bc9e18c1c396748bbec2b3d156f
sasikanthsai <128284394+sasikanthsai@users.noreply.github.com>
2026-06-20 09:41:13 +0530

    Add files via upload

---
+++ Design-Principles-Patterns/Exercise-2-Factory-Method-Pattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DocumentFactory factory;
+
+        factory = new WordFactory();
+        IDocument word = factory.CreateDocument();
+        word.Open();
+
+        factory = new PdfFactory();
+        IDocument pdf = factory.CreateDocument();
+        pdf.Open();
+
+        factory = new ExcelFactory();
+        IDocument excel = factory.CreateDocument();
+        excel.Open();
+    }
+}
